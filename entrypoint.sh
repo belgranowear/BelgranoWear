95a66fcbb631f235da31ebee087c1d413bc04435
FacuM <facumo.fm@gmail.com>
2024-02-21 00:22:22 -0300

    Automate artifact generation through GitHub Actions

---
+++ entrypoint.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Add Android SDK binaries to the global PATH
+export PATH="$PATH":/tmp/android/sdk/cmdline-tools/latest/bin;
+
+# Define the path to the Android SDK
+export ANDROID_HOME=/tmp/android/sdk;
+
+set -e; # quit on error
+
+echo '=> Installing Yarn dependencies...';
+yarn;
+
+echo '=> Installing packages with NPM...';
+npm i;
+
+echo '=> Looking for keystore...';
+if [[ -z $RELEASE_KEYSTORE ]]; then
+    echo 'Missing keystore!';
+
+    exit 1;
+fi;
+
+echo '=> Storing temporary encrypted keystore...';
+printf -- "$RELEASE_KEYSTORE" > /tmp/keystore.asc;
+
+echo '=> Decrypting keystore...';
+gpg -d --passphrase "$RELEASE_KEYSTORE_PASSPHRASE" --batch /tmp/keystore.asc > /tmp/keystore
+
+echo '=> Storing temporary ~/gradle.properties file...';
+mkdir -p ~/.gradle && printf -- "$GRADLE_PROPERTIES" > ~/.gradle/gradle.properties;
+
+# echo '=> Installing the Android SDK platform and build tools...';
+yes | sdkmanager 'build-tools;34.0.0';
+yes | sdkmanager 'platform-tools';
+
+# echo '=> Accepting all SDK licenses...';
+yes | sdkmanager --licenses;
+
+# echo '=> Complling debug AAB bundle and APK...';
+npx react-native build-android --mode=debug;
+cd android; ./gradlew assembleDebug; cd ..;
+
+# echo '=> Complling release AAB bundle and APK...';
+npx react-native build-android --mode=release;
+cd android; ./gradlew assembleRelease; cd ..;
+
+# Collect information for the release
+LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%s);
+LAST_COMMIT_SHA=$(git log -1 --pretty=%h);
+
+echo '=> Creating release...';
+RESPONSE=$(
+    curl -L \
+        --fail \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+        -d '{
+            "tag_name":"'"$LAST_COMMIT_SHA"'",
+            "target_commitish":"master",
+            "name":"'"$LAST_COMMIT_SHA"'",
+            "body":"'"$LAST_COMMIT_MESSAGE"'",
+            "draft":false,
+            "prerelease":false,
+            "generate_release_notes":false
+        }'
+);
+
+echo '=> Parsing upload URL...';
+RELEASE_ID=$(echo "$RESPONSE" | jq -cr '.id');
+UPLOAD_URL=$(echo "$RESPONSE" | jq -cr '.upload_url' | cut -d'{' -f1);
+
+echo '=> Uploading artifacts to release #'"$RELEASE_ID"' using the endpoint "'"$UPLOAD_URL"'"...';
+for file in $(find ./android/app/build/outputs -type f -regex  '.*\(apk\|aab\)$'); do
+    FILE_NAME=$(basename "$file");
+
+    echo '  -> Uploading "'"$file"'" as "'"$FILE_NAME"'"...';
+
+    curl -s -L \
+        -X POST \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H 'Accept: application/vnd.github.v3+json' \
+        -H 'Content-Type: '$(file -b --mime-type "$file") \
+        -H 'Content-Length: '$(wc -c < "$file" | xargs) \
+        -T "$file" \
+        "$UPLOAD_URL"'?name='"$FILE_NAME";
+done;
