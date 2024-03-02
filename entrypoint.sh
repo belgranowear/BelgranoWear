a3d4b156125bc21f126cfb92f9eb336cd8b50285
FacuM <facumo.fm@gmail.com>
2024-03-02 00:16:11 -0300

    Use a dynamic AndroidManifest to manage WearOS-specific releases

--- entrypoint.sh
+++ entrypoint.sh
@@ -94,3 +94,31 @@ for file in $(find ./android/app/build/outputs -type f -regex  '.*\(apk\|aab\)$'
         -T "$file" \
         "$UPLOAD_URL"'?name='"$FILE_NAME";
 done;
+
+echo '=> Setting WearOS releases...';
+sed -i'' 's/<!-- WEAROS_USES_FEATURE_PLACEHOLDER -->/<uses-feature android:name="android.hardware.type.watch" \/>/' android/app/src/main/AndroidManifest.xml;
+sed -i'' 's/<!-- WEAROS_META_PLACEHOLDER -->/<meta-data android:name="com.google.android.wearable.standalone" android:value="true"\/>/' android/app/src/main/AndroidManifest.xml;
+
+echo '=> Compilling debug AAB bundle and APK...';
+npx react-native build-android --mode=debug;
+cd android; ./gradlew assembleDebug; cd ..;
+
+echo '=> Compilling release AAB bundle and APK...';
+npx react-native build-android --mode=release;
+cd android; ./gradlew assembleRelease; cd ..;
+
+echo '=> Uploading artifacts to release #'"$RELEASE_ID"' using the endpoint "'"$UPLOAD_URL"'"...';
+for file in $(find ./android/app/build/outputs -type f -regex  '.*\(apk\|aab\)$'); do
+    FILE_NAME=$(basename "$file" | sed 's/\./-wear./');
+
+    echo '  -> Uploading "'"$file"'" as "'"$FILE_NAME"'"...';
+
+    curl -s -L \
+        -X POST \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H 'Accept: application/vnd.github.v3+json' \
+        -H 'Content-Type: '$(file -b --mime-type "$file") \
+        -H 'Content-Length: '$(wc -c < "$file" | xargs) \
+        -T "$file" \
+        "$UPLOAD_URL"'?name='"$FILE_NAME";
+done;
