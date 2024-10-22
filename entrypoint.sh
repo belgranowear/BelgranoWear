817f5a5d48fa77c95701cc9d1ca5d88b10229dc6
FacuM <facumo.fm@gmail.com>
2024-10-22 04:14:17 +0000

    entrypoint: enable verbose yarn and npm installs

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,10 @@ export WEAROS_TEMP_PATH=$(pwd)/wearos;
 set -e; # quit on error
 
 echo '=> Installing Yarn dependencies...';
-yarn;
+yarn --verbose;
 
 echo '=> Installing packages with NPM...';
-npm i;
+npm i --verbose;
 
 if [[ "$ACTION" == 'run' ]]; then
     npx expo start --clear;
